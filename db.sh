18a27a95f94f95528961cc641541213855fb169a
nileshgaikwad <nileshgaikwad@users.noreply.github.com>
2016-08-31 11:56:55 +0530

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 mysql -u root -proot -e "grant all privileges on db_devops.* to root@'%' identified by 'root';"
 mysql -u root -proot -e "grant all privileges on db_devops.* to root@localhost identified by 'root';"
+
+echo "Updating mysql configs in /etc/mysql/my.cnf."
+sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+echo "Updated mysql bind address in /etc/mysql/my.cnf to 0.0.0.0 to allow external connections."
+sudo service mysql stop
+sudo service mysql start
